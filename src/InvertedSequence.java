2c12813cc9ef816b04ed7dc62a4f583b4f3202cb
Daiii <longpanda@aliyun.com>
2017-12-22 14:56:16 +0800

    优化

--- src/InvertedSequence.java
+++ src/InvertedSequence.java
@@ -6,21 +6,26 @@
 public class InvertedSequence {
     public static void main(String[] args) {
         int[] arrayNumber = {1, 2, 3, 4, 5, 8, 7, 0};
-        printArray(reverse(arrayNumber));
+        printArray("before", arrayNumber);
+        printArray("after", reverse(arrayNumber));
     }
 
     private static int[] reverse(int[] arrayNumber) {
-        for (int i = 0; i < arrayNumber.length / 2; i++) {
-            int temp = arrayNumber[i];
-            arrayNumber[i] = arrayNumber[arrayNumber.length - 1 - i];
-            arrayNumber[arrayNumber.length - 1 - i] = temp;
+        int[] calcArray = arrayNumber;
+        for (int i = 0; i < calcArray.length / 2; i++) {
+            int temp = calcArray[i];
+            calcArray[i] = calcArray[calcArray.length - 1 - i];
+            calcArray[calcArray.length - 1 - i] = temp;
         }
-        return arrayNumber;
+        return calcArray;
     }
 
-    private static void printArray(int[] arrayNumber) {
-        for (int num : arrayNumber) {
-            System.out.println(num);
+    private static void printArray(String status, int[] arrayNumber) {
+        StringBuilder builder = new StringBuilder(status + " : ");
+        for (int item : arrayNumber) {
+            builder.append(item);
+            builder.append(" ");
         }
+        System.out.println(builder.toString());
     }
 }
